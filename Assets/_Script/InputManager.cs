3d0a8f3dfe80c3dd2ccc815005c9265817f623f1
Artjom Rozkov <artjom.rozhkov123@gmail.com>
2024-02-15 13:22:07 +0200

    Adding a script InputManager.cs

--- Assets/_Script/InputManager.cs
+++ Assets/_Script/InputManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Camera sceneCamera;
+
+    private Vector3 lastPosition;
+
+    [SerializeField]
+    private LayerMask placementLayermask;
+
+    public event Action OnClicked, OnExit;
+
+    private void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+            OnClicked?.Invoke();
+        if(Input.GetKeyDown(KeyCode.Escape))
+            OnExit?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsPointerOverUI()
+        => EventSystem.current.IsPointerOverGameObject();
+
+    public Vector3 GetSelectedMapPosition()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = sceneCamera.nearClipPlane;
+        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
+        {
+            lastPosition = hit.point;
+        }
+        return lastPosition;
     }
 }
